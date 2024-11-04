e30148fbd082b126a01e3289081aeda211a75085
abdlalisalmi <abdlalisalmi@gmail.com>
2024-11-04 22:57:30 +0100

    Fixed -- fix the staticfiles issue

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -146,7 +146,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_DIRS = [BASE_DIR / "static", BASE_DIR / "frontend" / "static"]
+# Additional directories where Django will look for static files
+STATICFILES_DIRS = [
+    BASE_DIR / "frontend" / "styles",
+]
+STATIC_DIRS = [BASE_DIR / "static", BASE_DIR / "frontend" / "styles"]
 STATIC_ROOT = BASE_DIR / "static"
 
 # MEDIA_URL = "media/" if not DEBUG else "http://localhost:8000/media/"
